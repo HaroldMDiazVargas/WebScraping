fe47da471a8dc20b9de7e509794590b883d9a5b0
Harold Diaz <harold.diazv1@gmail.com>
2022-10-24 10:11:57 -0500

    Class for tramites requisite

---
+++ menus/transparencia/tramites.py
@@ -0,0 +1,52 @@
+from requisites import abstractBase
+from utils.utilsFuncionts import getTextData
+
+# 32 Trámites y servicios
+class TramitesServicios(abstractBase.Requisites):
+    def checkRequisites(self, soup, browser, newUrl):
+        onPage = False
+        date = "-"
+        title = "-"
+        info = "-"
+
+        if (newUrl):
+            onPage = True
+            info = "URL Externa"
+            return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+        
+        elif(soup.select_one(".tramite-row")):
+            date = getTextData(soup.select_one(".tramite-row").select_one(".tramite__date"))
+            title = getTextData(soup.select_one(".tramite-row").select_one(".tramite__name"))
+            info = getTextData(soup.select_one(".tramite-row").select_one(".tramite__description"))
+            onPage = True
+
+        elif(soup.select_one(".content_text")):
+            date = getTextData(soup.select_one(".content_text").select_one("p[ng-bind$='date']"))
+            title = getTextData(soup.select_one(".content_text").select_one(".content-tit span"))
+            if (title == '-'):
+                title = getTextData(soup.select_one(".content_text").select_one(".docu--tit"))
+            info = getTextData(soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']"))
+            # info = soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']").getText()
+            onPage = True
+        
+        elif(soup.select_one(".section-tit")):
+            title = getTextData(soup.select_one(".section-tit"))
+            # title = soup.select_one(".section-tit").getText()
+            if (soup.select_one(".dateMc-wrap")):
+                date = soup.select_one(".dateMc-wrap").select("span")[0].string[-19:]
+
+            if(soup.select_one("p[ng-bind$='file.name']")):  
+                onPage = True
+                info = getTextData(soup.select_one("p[ng-bind$='file.name']"))
+
+            elif(soup.select_one(".content-descri")):
+                info =  getTextData(soup.select_one(".content-descri"))
+                onPage = True
+            else:
+                onPage = False
+                info="-"
+     
+            
+        # print(budgetDate)
+        return onPage, date, title, info[:self.maxCharacters], browser.current_url
