f9615c76337f6d35c1e91cd142264b9855a203b7
Harold Diaz <harold.diazv1@gmail.com>
2022-10-24 11:19:00 -0500

    Adding classes for grupo_interes requisites

---
+++ menus/transparencia/grupo_interes.py
@@ -0,0 +1,131 @@
+from requisites import abstractBase
+from utils.utilsFuncionts import getTextData
+
+#1 información para niños, niñas y adolescentes
+class InfoNiños(abstractBase.Requisites):
+    def checkRequisites(self, soup, browser, newUrl):
+        onPage = False
+        date = "-"
+        title = "-"
+        info = "-"
+
+        if (newUrl):
+            onPage = True
+            info = "URL Externa"
+            return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+        elif(soup.select_one(".content_text")):
+            date = getTextData(soup.select_one(".content_text").select_one("p[ng-bind$='date']"))
+            title = getTextData(soup.select_one(".content_text").select_one(".content-tit span"))
+            if (title == '-'):
+                title = getTextData(soup.select_one(".content_text").select_one(".docu--tit"))
+            info = getTextData(soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']"))
+            # info = soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']").getText()
+            onPage = True
+        
+        elif(soup.select_one(".section-tit")):
+            title = getTextData(soup.select_one(".section-tit"))
+            # title = soup.select_one(".section-tit").getText()
+            if (soup.select_one(".dateMc-wrap")):
+                date = soup.select_one(".dateMc-wrap").select("span")[0].string[-19:]
+
+            if(soup.select_one("p[ng-bind$='file.name']")):  
+                onPage = True
+                info = getTextData(soup.select_one("p[ng-bind$='file.name']"))
+
+            elif(soup.select_one(".content-descri")):
+                info =  getTextData(soup.select_one(".content-descri"))
+                onPage = True
+            else:
+                onPage = False
+                info="-"
+     
+            
+        # print(budgetDate)
+        return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+#2 información para mujeres
+class InfoMujeres(abstractBase.Requisites):
+    def checkRequisites(self, soup, browser, newUrl):
+        onPage = False
+        date = "-"
+        title = "-"
+        info = "-"
+
+        if (newUrl):
+            onPage = True
+            info = "URL Externa"
+            return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+        elif(soup.select_one(".content_text")):
+            date = getTextData(soup.select_one(".content_text").select_one("p[ng-bind$='date']"))
+            title = getTextData(soup.select_one(".content_text").select_one(".content-tit span"))
+            if (title == '-'):
+                title = getTextData(soup.select_one(".content_text").select_one(".docu--tit"))
+            info = getTextData(soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']"))   #TODO: Sibling content-tit ????  docu--tit + p  OR docu--tit (p[meta AWAY!])
+            # info = soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']").getText()
+            onPage = True
+        
+        elif(soup.select_one(".section-tit")):
+            title = getTextData(soup.select_one(".section-tit"))
+            # title = soup.select_one(".section-tit").getText()
+            if (soup.select_one(".dateMc-wrap")):
+                date = soup.select_one(".dateMc-wrap").select("span")[0].string[-19:]
+
+            if(soup.select_one("p[ng-bind$='file.name']")):  
+                onPage = True
+                info = getTextData(soup.select_one("p[ng-bind$='file.name']"))
+
+            elif(soup.select_one(".content-descri")):
+                info =  getTextData(soup.select_one(".content-descri"))
+                onPage = True
+            else:
+                onPage = False
+                info="-"
+     
+            
+        # print(budgetDate)
+        return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+#2 información adicional
+class InfoMujeres(abstractBase.Requisites):
+    def checkRequisites(self, soup, browser, newUrl):
+        onPage = False
+        date = "-"
+        title = "-"
+        info = "-"
+
+        if (newUrl):
+            onPage = True
+            info = "URL Externa"
+            return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+        elif(soup.select_one(".content_text")):
+            date = getTextData(soup.select_one(".content_text").select_one("p[ng-bind$='date']"))
+            title = getTextData(soup.select_one(".content_text").select_one(".content-tit span"))
+            if (title == '-'):
+                title = getTextData(soup.select_one(".content_text").select_one(".docu--tit"))
+            info = getTextData(soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']"))   
+            # info = soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']").getText()
+            onPage = True
+        
+        elif(soup.select_one(".section-tit")):
+            title = getTextData(soup.select_one(".section-tit"))
+            # title = soup.select_one(".section-tit").getText()
+            if (soup.select_one(".dateMc-wrap")):
+                date = soup.select_one(".dateMc-wrap").select("span")[0].string[-19:]
+
+            if(soup.select_one("p[ng-bind$='file.name']")):  
+                onPage = True
+                info = getTextData(soup.select_one("p[ng-bind$='file.name']"))
+
+            elif(soup.select_one(".content-descri")):
+                info =  getTextData(soup.select_one(".content-descri"))
+                onPage = True
+            else:
+                onPage = False
+                info="-"
+     
+            
+        # print(budgetDate)
+        return onPage, date, title, info[:self.maxCharacters], browser.current_url
