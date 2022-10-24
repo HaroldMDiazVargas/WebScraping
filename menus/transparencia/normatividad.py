ca96be88d3a0c6a19c2259f588b8eebb3b540d94
Harold Diaz <harold.diazv1@gmail.com>
2022-10-24 13:53:05 -0500

    Update normatividad file class

--- menus/transparencia/normatividad.py
+++ menus/transparencia/normatividad.py
@@ -2,7 +2,9 @@
 from requisites import abstractBase
 from utils.utilsFuncionts import getTextData
 
-# 15 Normatividad de la entidad
+
+
+# 1 Normatividad de la entidad
 class NormatividadEntidad(abstractBase.Requisites):
     def checkRequisites(self, soup, browser, newUrl):
         onPage = False
@@ -52,8 +54,94 @@ class NormatividadEntidad(abstractBase.Requisites):
         # print(budgetDate)
         return onPage, date, title, info[:self.maxCharacters], browser.current_url
 
+#2 Vinculo al diario o gaceta oficial
+class VinculoGacetaOficial(abstractBase.Requisites):
+    def checkRequisites(self, soup, browser, newUrl):
+        onPage = False
+        date = "-"
+        title = "-"
+        info = "-"
 
-#16 Busqueda de Normas
+        if (newUrl):
+            onPage = True
+            info = "URL Externa"
+            return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+
+        elif(soup.select_one(".content_text")):
+            date = getTextData(soup.select_one(".content_text").select_one("p[ng-bind$='date']"))
+            title = getTextData(soup.select_one(".content_text").select_one(".content-tit span"))
+            if (title == '-'):
+                title = getTextData(soup.select_one(".content_text").select_one(".docu--tit"))
+            info = getTextData(soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']"))
+            # info = soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']").getText()
+            onPage = True
+        
+        elif(soup.select_one(".section-tit")):
+            title = getTextData(soup.select_one(".section-tit"))
+            # title = soup.select_one(".section-tit").getText()
+            if (soup.select_one(".dateMc-wrap")):
+                date = soup.select_one(".dateMc-wrap").select("span")[0].string[-19:]
+
+            if(soup.select_one("p[ng-bind$='file.name']")):  
+                onPage = True
+                info = getTextData(soup.select_one("p[ng-bind$='file.name']"))
+
+            elif(soup.select_one(".content-descri")):
+                info =  getTextData(soup.select_one(".content-descri"))
+                onPage = True
+            else:
+                onPage = False
+                info="-"
+            
+        # print(budgetDate)
+        return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+#3 Politicas, lineamientos y manuales
+class PoliticaLineamientos(abstractBase.Requisites):
+    def checkRequisites(self, soup, browser, newUrl):
+        onPage = False
+        date = "-"
+        title = "-"
+        info = "-"
+
+        if (newUrl):
+            onPage = True
+            info = "URL Externa"
+            return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+        elif(soup.select_one(".content_text")):
+            date = getTextData(soup.select_one(".content_text").select_one("p[ng-bind$='date']"))
+            title = getTextData(soup.select_one(".content_text").select_one(".content-tit span"))
+            if (title == '-'):
+                title = getTextData(soup.select_one(".content_text").select_one(".docu--tit"))
+            info = getTextData(soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']"))
+            # info = soup.select_one(".content_text").select_one("p[ng-bind-html$='metaDescription']").getText()
+            onPage = True
+        
+        elif(soup.select_one(".section-tit")):
+            title = getTextData(soup.select_one(".section-tit"))
+            # title = soup.select_one(".section-tit").getText()
+            if (soup.select_one(".dateMc-wrap")):
+                date = soup.select_one(".dateMc-wrap").select("span")[0].string[-19:]
+
+            if(soup.select_one("p[ng-bind$='file.name']")):  
+                onPage = True
+                info = getTextData(soup.select_one("p[ng-bind$='file.name']"))
+
+            elif(soup.select_one(".content-descri")):
+                info =  getTextData(soup.select_one(".content-descri"))
+                onPage = True
+            else:
+                onPage = False
+                info="-"
+            
+        # print(budgetDate)
+        return onPage, date, title, info[:self.maxCharacters], browser.current_url
+
+
+
+#4 Busqueda de Normas
 class BusquedaNormas(abstractBase.Requisites):
     def checkRequisites(self, soup, browser, newUrl):
         onPage = False
@@ -108,7 +196,7 @@ class BusquedaNormas(abstractBase.Requisites):
         return onPage, date, title, info[:self.maxCharacters], browser.current_url
 
 
-#17 Proyecto de normas para comentarios
+#5 Proyecto de normas para comentarios
 class NormasParaComentarios(abstractBase.Requisites):
     def checkRequisites(self, soup, browser, newUrl):
         onPage = False
